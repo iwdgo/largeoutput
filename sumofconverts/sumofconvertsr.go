b36c1a2b1c36a22b4d2b0a58978a77fe4a5742e6
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2022-11-03 09:30:02 +0100

    Sum of converts: simplify if tree

--- sumofconverts/sumofconvertsr.go
+++ sumofconverts/sumofconvertsr.go
@@ -8,11 +8,11 @@ func stringstoint(restofstrings []string) int64 {
 	i, err := strconv.ParseInt(restofstrings[0], 10, 64)
 	if err == nil {
 		return i + stringstoint(restofstrings[1:])
-	} else if len(restofstrings) == 1 {
-		return 0
-	} else {
-		return stringstoint(restofstrings[1:])
 	}
+	if len(restofstrings) == 1 {
+		return 0
+	}
+	return stringstoint(restofstrings[1:])
 }
 
 // sumOfConvertsR is the recursive version of sumOfConverts.
