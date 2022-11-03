ba89b5ed6feb9edb647075884d6f4744569a672a
Constantin Konstantinidis <ConstantinKonstantinidis@gmail.com>
2022-11-03 09:31:57 +0100

    Sum of converts: move solutions to one file

--- sumofconverts/sumofconverts.go
+++ sumofconverts/sumofconverts.go
@@ -41,3 +41,19 @@ func sumOfConverts() (sum int64) {
 	}
 	return
 }
+
+func stringstoint(restofstrings []string) int64 {
+	i, err := strconv.ParseInt(restofstrings[0], 10, 64)
+	if err == nil {
+		return i + stringstoint(restofstrings[1:])
+	}
+	if len(restofstrings) == 1 {
+		return 0
+	}
+	return stringstoint(restofstrings[1:])
+}
+
+// sumOfConvertsR is the recursive version of sumOfConverts.
+func sumOfConvertsR() int64 {
+	return stringstoint(listofstrings)
+}
